f52ca6bbe70a6f38421b6a5faa3dbea27706d5eb
Gonzalo Gauna <79806700+GonzaloGauna@users.noreply.github.com>
2022-11-24 19:37:15 -0300

    adding resize function for responsive components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'StarWarsDocs';
+  isMobile! : boolean;
+  resizeObservable$!: Observable<Event>;
+  resizeSubscription$!: Subscription;
+
+  
+  ngOnInit(): void {
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+      const screenRes = screen.width;
+      if(screenRes <= 600){
+        this.isMobile = true;
+      }else {
+        this.isMobile = false;
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.resizeSubscription$.unsubscribe()
+}
 }
